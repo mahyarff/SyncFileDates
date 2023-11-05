5296e453cd50b85726e2e1c9b4d2d28b3342039f
Mahyar <mahyarff@users.noreply.github.com>
2023-11-05 10:20:08 +0330

    Add files via upload
    First Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace SyncFileDate
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txb_SourcePath = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_BrowseSrc = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_BrowseDst = new System.Windows.Forms.Button();
+            this.txb_DestinationPath = new System.Windows.Forms.TextBox();
+            this.txb_Result = new System.Windows.Forms.TextBox();
+            this.btn_Sync = new System.Windows.Forms.Button();
+            this.txb_Pattern = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chb_Recursive = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txb_SourcePath
+            // 
+            this.txb_SourcePath.AllowDrop = true;
+            this.txb_SourcePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txb_SourcePath.Location = new System.Drawing.Point(6, 19);
+            this.txb_SourcePath.Name = "txb_SourcePath";
+            this.txb_SourcePath.Size = new System.Drawing.Size(764, 20);
+            this.txb_SourcePath.TabIndex = 0;
+            this.txb_SourcePath.DragDrop += new System.Windows.Forms.DragEventHandler(this.txb_SourcePath_DragDrop);
+            this.txb_SourcePath.DragEnter += new System.Windows.Forms.DragEventHandler(this.txb_SourcePath_DragEnter);
+            this.txb_SourcePath.DragOver += new System.Windows.Forms.DragEventHandler(this.txb_SourcePath_DragOver);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btn_BrowseSrc);
+            this.groupBox1.Controls.Add(this.txb_SourcePath);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 77);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source";
+            // 
+            // btn_BrowseSrc
+            // 
+            this.btn_BrowseSrc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_BrowseSrc.Location = new System.Drawing.Point(695, 45);
+            this.btn_BrowseSrc.Name = "btn_BrowseSrc";
+            this.btn_BrowseSrc.Size = new System.Drawing.Size(75, 23);
+            this.btn_BrowseSrc.TabIndex = 1;
+            this.btn_BrowseSrc.Text = "Browse";
+            this.btn_BrowseSrc.UseVisualStyleBackColor = true;
+            this.btn_BrowseSrc.Click += new System.EventHandler(this.btn_BrowseSrc_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btn_BrowseDst);
+            this.groupBox2.Controls.Add(this.txb_DestinationPath);
+            this.groupBox2.Location = new System.Drawing.Point(12, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 77);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Destination";
+            // 
+            // btn_BrowseDst
+            // 
+            this.btn_BrowseDst.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_BrowseDst.Location = new System.Drawing.Point(695, 45);
+            this.btn_BrowseDst.Name = "btn_BrowseDst";
+            this.btn_BrowseDst.Size = new System.Drawing.Size(75, 23);
+            this.btn_BrowseDst.TabIndex = 1;
+            this.btn_BrowseDst.Text = "Browse";
+            this.btn_BrowseDst.UseVisualStyleBackColor = true;
+            this.btn_BrowseDst.Click += new System.EventHandler(this.btn_BrowseDst_Click);
+            // 
+            // txb_DestinationPath
+            // 
+            this.txb_DestinationPath.AllowDrop = true;
+            this.txb_DestinationPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txb_DestinationPath.Location = new System.Drawing.Point(6, 19);
+            this.txb_DestinationPath.Name = "txb_DestinationPath";
+            this.txb_DestinationPath.Size = new System.Drawing.Size(764, 20);
+            this.txb_DestinationPath.TabIndex = 0;
+            this.txb_DestinationPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.txb_DestinationPath_DragDrop);
+            this.txb_DestinationPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.txb_DestinationPath_DragEnter);
+            this.txb_DestinationPath.DragOver += new System.Windows.Forms.DragEventHandler(this.txb_DestinationPath_DragOver);
+            // 
+            // txb_Result
+            // 
+            this.txb_Result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txb_Result.BackColor = System.Drawing.SystemColors.Window;
+            this.txb_Result.Location = new System.Drawing.Point(12, 200);
+            this.txb_Result.Multiline = true;
+            this.txb_Result.Name = "txb_Result";
+            this.txb_Result.ReadOnly = true;
+            this.txb_Result.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txb_Result.Size = new System.Drawing.Size(776, 211);
+            this.txb_Result.TabIndex = 3;
+            // 
+            // btn_Sync
+            // 
+            this.btn_Sync.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Sync.Location = new System.Drawing.Point(713, 417);
+            this.btn_Sync.Name = "btn_Sync";
+            this.btn_Sync.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sync.TabIndex = 4;
+            this.btn_Sync.Text = "Sync";
+            this.btn_Sync.UseVisualStyleBackColor = true;
+            this.btn_Sync.Click += new System.EventHandler(this.btn_Sync_Click);
+            // 
+            // txb_Pattern
+            // 
+            this.txb_Pattern.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txb_Pattern.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txb_Pattern.Location = new System.Drawing.Point(62, 417);
+            this.txb_Pattern.Name = "txb_Pattern";
+            this.txb_Pattern.Size = new System.Drawing.Size(82, 22);
+            this.txb_Pattern.TabIndex = 5;
+            this.txb_Pattern.Text = "*.dll";
+            this.txb_Pattern.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Pattern";
+            // 
+            // chb_Recursive
+            // 
+            this.chb_Recursive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chb_Recursive.AutoSize = true;
+            this.chb_Recursive.Checked = true;
+            this.chb_Recursive.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chb_Recursive.Location = new System.Drawing.Point(166, 420);
+            this.chb_Recursive.Name = "chb_Recursive";
+            this.chb_Recursive.Size = new System.Drawing.Size(74, 17);
+            this.chb_Recursive.TabIndex = 7;
+            this.chb_Recursive.Text = "Recursive";
+            this.chb_Recursive.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chb_Recursive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txb_Pattern);
+            this.Controls.Add(this.btn_Sync);
+            this.Controls.Add(this.txb_Result);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "Sync File Dates";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txb_SourcePath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_BrowseSrc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_BrowseDst;
+        private System.Windows.Forms.TextBox txb_DestinationPath;
+        private System.Windows.Forms.TextBox txb_Result;
+        private System.Windows.Forms.Button btn_Sync;
+        private System.Windows.Forms.TextBox txb_Pattern;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chb_Recursive;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace SyncFileDate
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Sync_Click(object sender, EventArgs e)
+        {
+            string src = txb_SourcePath.Text.Trim(), dst = txb_DestinationPath.Text.Trim();
+            if (!Directory.Exists(src) || !Directory.Exists(dst))
+            {
+                // show dialog
+                return;
+            }
+
+            string pattern = string.IsNullOrWhiteSpace(txb_Pattern.Text) ? "*" : txb_Pattern.Text.Trim();
+            SearchOption searchOption = chb_Recursive.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            DisableControls();
+            Thread thread = new Thread(() =>
+            {
+                string[] dstFiles = Directory.GetFiles(dst, pattern, searchOption);
+                string[] srcFiles = Directory.GetFiles(src, pattern, searchOption);
+                List<FileData> dstFileDatas = new List<FileData>(dstFiles.Select(y => new FileData(y)));
+                List<FileData> srcFileDatas = new List<FileData>(srcFiles.Select(x => new FileData(x)));
+                //DateTime lastWriteTime, creationTime, lastAccessTime;
+                foreach (var fileData in dstFileDatas)
+                {
+                    try
+                    {
+                        var found = FileData.FindEqualivantFile(fileData, srcFileDatas);
+                        if (found != null)
+                        {
+                            var fileInfo = new FileInfo(fileData.Path);
+                            fileInfo.LastAccessTime = found.LastAccessTime;
+                            fileInfo.CreationTime = found.CreationTime;
+                            fileInfo.LastWriteTime = found.LastWriteTime;
+
+                            txb_Result.InvokeIfRequired(x => x.AppendText($"{fileData.Path} processed.\r\n"));
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                DisableControls(false);
+            });
+            thread.Start();
+        }
+
+        private void DisableControls(bool disable = true)
+        {
+            List<Control> controls = new List<Control>();
+            controls.Add(txb_SourcePath);
+            controls.Add(txb_DestinationPath);
+            controls.Add(txb_Pattern);
+            controls.Add(chb_Recursive);
+            controls.Add(btn_Sync);
+            controls.Add(btn_BrowseSrc);
+            controls.Add(btn_BrowseDst);
+            controls.ForEach(x => x.InvokeIfRequired(y => y.Enabled = !disable));
+            controls.Clear();
+        }
+
+
+
+        private void txb_SourcePath_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 1 && !Directory.Exists(txb_SourcePath.Text = files[0]))
+            {
+                txb_SourcePath.Text = string.Empty;
+            }
+        }
+
+        private void txb_SourcePath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void txb_SourcePath_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void txb_DestinationPath_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 1 && !Directory.Exists(txb_DestinationPath.Text = files[0]))
+            {
+                txb_DestinationPath.Text = string.Empty;
+            }
+        }
+
+        private void txb_DestinationPath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void txb_DestinationPath_DragOver(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void btn_BrowseSrc_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog openfolderdlg = new CommonOpenFileDialog();
+            openfolderdlg.IsFolderPicker = true;
+            openfolderdlg.RestoreDirectory = true;
+
+            if (openfolderdlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txb_SourcePath.Text = openfolderdlg.FileName;
+            }
+        }
+
+        private void btn_BrowseDst_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog openfolderdlg = new CommonOpenFileDialog();
+            openfolderdlg.IsFolderPicker = true;
+            openfolderdlg.RestoreDirectory = true;
+
+            if (openfolderdlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                txb_DestinationPath.Text = openfolderdlg.FileName;
+            }
+        }
+    }
+
+    public class FileData
+    {
+        private Version _version;
+        private string _name;
+        private string _fullName;
+
+        public FileData()
+        {
+        }
+
+        public FileData(string path)
+        {
+            Path = path;
+            FileInfo fileInfo = new FileInfo(path);
+            if (!(IsAssembly = IsValidAssembly(path, ref _name, ref _version, ref _fullName)))
+            {
+                _name = fileInfo.Name;
+            }
+
+            LastAccessTime = fileInfo.LastAccessTime;
+            CreationTime = fileInfo.CreationTime;
+            LastWriteTime = fileInfo.LastWriteTime;
+        }
+
+        public string Path { get; set; }
+        public bool IsAssembly { get; set; }
+        public Version Version
+        {
+            get { return _version; }
+            set { _version = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string FullName
+        {
+            get { return _fullName; }
+            set { _fullName = value; }
+        }
+        public DateTime LastAccessTime { get; set; } = DateTime.MinValue;
+        public DateTime CreationTime { get; set; } = DateTime.MinValue;
+        public DateTime LastWriteTime { get; set; } = DateTime.MinValue;
+
+        public static bool IsValidAssembly(string path, ref string name, ref Version version, ref string fullName)
+        {
+            try
+            {
+                var assembly = AssemblyName.GetAssemblyName(path);
+                name = assembly.Name;
+                version = assembly.Version;
+                fullName = assembly.FullName;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static FileData FindEqualivantFile(FileData dstFiledata, IEnumerable<FileData> srcFileDatas)
+        {
+            foreach (var fileData in srcFileDatas)
+            {
+                if (dstFiledata.Name.Equals(fileData.Name, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (dstFiledata.IsAssembly && fileData.IsAssembly &&
+                        dstFiledata.Version.Equals(fileData.Version) &&
+                        dstFiledata.FullName.Equals(fileData.FullName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return fileData;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+    public static class ControlHelper
+    {
+        public static void InvokeIfRequired<T>(this T control, Action<T> action) where T : ISynchronizeInvoke
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new Action(() => action(control)), null);
+            }
+            else
+            {
+                action(control);
+            }
+        }
+    }
+}
